8593384f695c4b86068cabc45427507a65f4937c
unknown <polak.piotr94@gmail.com>
2022-10-28 04:06:33 +0200

    Initial commit

---
+++ HelloWorld_CS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Welcome
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
